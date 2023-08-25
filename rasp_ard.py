43e7e0d2395bb70001229ac4255ad800151b4048
rorro128 <rodrigo.tardone@gmail.com>
2023-08-24 22:07:00 -0400

    6 first commint

---
+++ rasp_ard.py
@@ -0,0 +1,23 @@
+import time
+import serial
+
+
+ser = serial.Serial("/dev/ttyACM0", baudrate=9600) #Modificar el puerto serie de ser necesario
+
+try:
+    while True:
+        comando = input("Ingresar comando (on/off): ")
+        comando = comando + "\n"
+        comandoBytes = comando.encode()
+        ser.write(comandoBytes)
+        time.sleep(0.1)
+        read = ser.readline()
+        print(read)
+
+except KeyboardInterrupt:
+    print("\nInterrupcion por teclado")
+except ValueError as ve:
+    print(ve)
+    print("Otra interrupcion")
+finally:
+    ser.close()
